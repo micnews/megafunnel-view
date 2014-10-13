1def6c68eb5dd8734e3eb6e331ca8dd55d7ba740
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-13 17:16:38 -0200

    test the new api

--- test/query.js
+++ test/query.js
@@ -2,14 +2,14 @@
 var q = require('../query')
 var tape = require('tape')
 
-var line = 'condor,1.1.1,trackable-custom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:{articleUnitId}'
+var line = 'condor,2.0.0,trackable-custom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:{articleUnitId}'
 
 tape('create count query', function (t) {
 
-  var reduce =  q.count('condor,1.1.1,trackable-custom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:{articleUnitId}')
+  var reduce =  q.count('condor,2.0.0,trackable-custom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:{articleUnitId}')
 
-  var line = 'condor,1.1.1,trackable-cusmom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:foo'
-  var line2 = 'condor,1.1.1,trackable-cusmom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:bar'
+  var line = 'condor,2.0.0,trackable-cusmom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:foo'
+  var line2 = 'condor,2.0.0,trackable-cusmom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:bar'
 
   var value = reduce(null, line)
   t.deepEqual({foo: 1}, value)
@@ -23,12 +23,14 @@ tape('create count query', function (t) {
   t.end()
 })
 
-tape('create stats query', function (t) {
-  var reduce = q.stats('condor,1.1.0,end,,,,,,{duration}')
 
-  var line = 'condor,1.1.0,end,782,757,0,2300,http://url.com/foo,20300,date,240,,,,,,,,,,0'
-  var line1 = 'condor,1.1.0,end,782,757,0,2300,http://url.com/foo,16300,date,240,,,,,,,,,,0'
-  var line2 = 'condor,1.1.0,end,782,757,0,2300,http://url.com/foo,33300,date,240,,,,,,,,,,0'
+
+tape('create stats query', function (t) {
+  var reduce = q.stats('condor,2.0.0,end,,,,,,{duration}')
+
+  var line = 'condor,2.0.0,end,782,757,0,2300,http://url.com/foo,20300,date,240,,,,,,,,,,0'
+  var line1 = 'condor,2.0.0,end,782,757,0,2300,http://url.com/foo,16300,date,240,,,,,,,,,,0'
+  var line2 = 'condor,2.0.0,end,782,757,0,2300,http://url.com/foo,33300,date,240,,,,,,,,,,0'
 
   var value = reduce(null, line)
   t.equal(value.mean, 20300)
@@ -41,3 +43,27 @@ tape('create stats query', function (t) {
 
   t.end()
 })
+
+tape('create stats query', function (t) {
+  var reduce = q.stats({
+    clientName: 'condor',
+    eventName: 'end',
+    duration: true
+  })
+
+  var line = 'condor,2.0.0,end,782,757,0,2300,http://url.com/foo,20300,date,240,,,,,,,,,,0'
+  var line1 = 'condor,2.0.0,end,782,757,0,2300,http://url.com/foo,16300,date,240,,,,,,,,,,0'
+  var line2 = 'condor,2.0.0,end,782,757,0,2300,http://url.com/foo,33300,date,240,,,,,,,,,,0'
+
+  var value = reduce(null, line)
+  t.equal(value.mean, 20300)
+
+  var value1 = reduce(value, line1)
+  t.equal(value.mean, 18300)
+
+  var value2 = reduce(value1, line2)
+  t.equal(value.mean, 23300)
+
+  t.end()
+})
+
