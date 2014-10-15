68066e45e5b7c0e99e12efc90a6133f2c0be1710
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-15 14:05:09 -0200

    fix test

--- test/query.js
+++ test/query.js
@@ -2,14 +2,15 @@
 var q = require('../query')
 var tape = require('tape')
 
-var line = 'condor,2.0.0,trackable-custom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:{articleUnitId}'
+var ts = Date.now()
+var line = ts+',condor,2.0.0,trackable-custom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:{articleUnitId}'
 
 tape('create count query', function (t) {
 
   var reduce =  q.count('condor,2.0.0,trackable-custom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:{articleUnitId}')
 
-  var line = 'condor,2.0.0,trackable-cusmom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:foo'
-  var line2 = 'condor,2.0.0,trackable-cusmom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:bar'
+  var line = ts+',condor,2.0.0,trackable-cusmom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:foo'
+  var line2 = ts+',condor,2.0.0,trackable-cusmom,,,,,,,,,,,,,,,,,ed-ab-share,articleUnitId:bar'
 
   var value = reduce(null, line)
   t.deepEqual({foo: 1}, value)
@@ -28,9 +29,11 @@ tape('create count query', function (t) {
 tape('create stats query', function (t) {
   var reduce = q.stats('condor,2.0.0,end,,,,,,{duration}')
 
-  var line = 'condor,2.0.0,end,782,757,0,2300,http://url.com/foo,20300,date,240,,,,,,,,,,0'
-  var line1 = 'condor,2.0.0,end,782,757,0,2300,http://url.com/foo,16300,date,240,,,,,,,,,,0'
-  var line2 = 'condor,2.0.0,end,782,757,0,2300,http://url.com/foo,33300,date,240,,,,,,,,,,0'
+  var ts = Date.now()
+
+  var line = ts+',condor,2.0.0,end,782,757,0,2300,http://url.com/foo,20300,date,240,,,,,,,,,,0'
+  var line1 = ts+',condor,2.0.0,end,782,757,0,2300,http://url.com/foo,16300,date,240,,,,,,,,,,0'
+  var line2 = ts+',condor,2.0.0,end,782,757,0,2300,http://url.com/foo,33300,date,240,,,,,,,,,,0'
 
   var value = reduce(null, line)
   t.equal(value.mean, 20300)
@@ -50,10 +53,11 @@ tape('create stats query - legacy', function (t) {
     eventName: 'end',
     duration: true
   })
+  var ts = Date.now()
 
-  var line = 'end,782,757,0,2300,http://url.com/foo,20300,date,240,,,,,,,,,,0'
-  var line1 = 'end,782,757,0,2300,http://url.com/foo,16300,date,240,,,,,,,,,,0'
-  var line2 = 'end,782,757,0,2300,http://url.com/foo,33300,date,240,,,,,,,,,,0'
+  var line = ts+',end,782,757,0,2300,http://url.com/foo,20300,date,240,,,,,,,,,,0'
+  var line1 = ts+',end,782,757,0,2300,http://url.com/foo,16300,date,240,,,,,,,,,,0'
+  var line2 = ts+',end,782,757,0,2300,http://url.com/foo,33300,date,240,,,,,,,,,,0'
 
   var value = reduce(null, line)
   t.equal(value.mean, 20300)
@@ -73,19 +77,20 @@ tape('create stats query', function (t) {
     eventName: 'end',
     duration: true
   })
+  var ts = Date.now()
 
-  var line = 'condor,2.0.0,end,782,757,0,2300,http://url.com/foo,20300,date,240,,,,,,,,,,0'
-  var line1 = 'condor,2.0.0,end,782,757,0,2300,http://url.com/foo,16300,date,240,,,,,,,,,,0'
-  var line2 = 'condor,2.0.0,end,782,757,0,2300,http://url.com/foo,33300,date,240,,,,,,,,,,0'
+  var line = ts+',condor,2.0.0,end,782,757,0,2300,http://url.com/foo,20300,date,240,,,,,,,,,,0'
+  var line1 = ts+',condor,2.0.0,end,782,757,0,2300,http://url.com/foo,16300,date,240,,,,,,,,,,0'
+  var line2 = ts+',condor,2.0.0,end,782,757,0,2300,http://url.com/foo,33300,date,240,,,,,,,,,,0'
 
   var value = reduce(null, line)
   t.equal(value.mean, 20300)
 
   var value1 = reduce(value, line1)
-  t.equal(value.mean, 18300)
+  t.equal(value1.mean, 18300)
 
   var value2 = reduce(value1, line2)
-  t.equal(value.mean, 23300)
+  t.equal(value2.mean, 23300)
 
   t.end()
 })
