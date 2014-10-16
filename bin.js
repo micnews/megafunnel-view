1c3b31ecd19945ca5baf0d11c0a37fe43b7f8ec3
Dominic Tarr <dominic.tarr@gmail.com>
2014-10-16 15:48:40 -0200

    run named queries from config

--- bin.js
+++ bin.js
@@ -1,56 +1,80 @@
-if(!module.parent) {
-  var split = require('split')
-  var opts = require('minimist')(process.argv.slice(2))
-  var through = require('through')
+#! /usr/bin/env node
 
-  var reduce
-  if(opts.count)
-    reduce = exports.count(opts.count)
-  else if(opts.stats)
-    reduce = exports.stats(opts.stats)
-  else
-    throw new Error('expects --count PATTERN or --stats PATTERN')
+var query = require('./query')
+var csvLine = require('csv-line')
 
-  var buckets = (opts.buckets || '')
-    .split(',').map(function (s) { return s.toUpperCase() })
+var split = require('split')
+var opts = require('./config')
+var through = require('through')
 
-  var TBR = require('time-bucket-reduce')({
-    map: function (line) {
-      return reduce.filter(line)
-    },
-    reduce: reduce.reduce,
-    output: function (value, start, part) {
-      if(~buckets.indexOf(part.toUpperCase()))
-        console.log('==', value, start, part)
-    }
-  })
+opts.buckets = opts.buckets || 'Minutes'
 
-  var acc = null
-
-  var p = Date.now()
-
-  var queryStream = opts.buckets
-  ? through(function (data) {
-    if(data.length) TBR(data)
-  }, function () {
-    console.log(TBR.dump())
-  })
-  : through(function (data) {
-      acc = reduce(acc, data)
-      var ts = Date.now()
-      if(p + 100 < ts) {
-        p = ts
-        console.log(acc)
-      }
-    }, function () {
-      console.log(acc)
-    })
-
-  process.stdin
-    .pipe(split())
-    .pipe(through(function (l) {
-      this.queue(csvLine.decode(l))
-    }))
-    .pipe(queryStream)
-
+function parse (p) {
+  try { p = JSON.parse(p) } catch (err) { }
+  return p
 }
+
+var reduce
+//perform a named query from the config file.
+if(opts.query) {
+  var q = opts.views[opts.query]
+  if(q.count)
+    reduce = query.count(q.count)
+  else if(q.stats)
+    reduce = query.stats(q.stats)
+  else
+    throw new Error('query:'+opts.query+' must have "count" or "stats" property')
+}
+else {
+
+if(opts.count)
+  reduce = query.count(parse(opts.count))
+else if(opts.stats)
+  reduce = query.stats(parse(opts.stats))
+else
+  throw new Error('expects --count PATTERN or --stats PATTERN or --query NAME')
+}
+
+var buckets = (opts.buckets || '')
+  .split(',').map(function (s) { return s.toUpperCase() })
+
+var TBR = require('time-bucket-reduce')({
+  map: function (line) {
+    return reduce.filter(line)
+  },
+  reduce: reduce.reduce,
+  output: function (value, start, part) {
+    if(~buckets.indexOf(part.toUpperCase()))
+      console.log('==', value, start, part)
+  }
+})
+
+var acc = null
+
+var p = Date.now()
+
+var queryStream = opts.buckets
+? through(function (data) {
+  if(data.length) TBR(data)
+}, function () {
+  console.log(TBR.dump())
+})
+: through(function (data) {
+    acc = reduce(acc, data)
+    var ts = Date.now()
+    if(p + 100 < ts) {
+      p = ts
+      console.log(acc)
+    }
+  }, function () {
+    console.log(acc)
+  })
+
+process.stdin
+  .pipe(split())
+  .pipe(through(function (l) {
+    this.queue(csvLine.decode(l))
+  }))
+  .pipe(queryStream)
+
+
